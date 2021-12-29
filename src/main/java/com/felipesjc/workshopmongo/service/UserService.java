7a5f5e3c5a69460f411f72b027e19ec10ce29cc1
Felipesjc17 <felipe_sjc17@hotmail.com>
2021-12-29 12:16:14 -0300

    Obtendo um usuario por id

--- src/main/java/com/felipesjc/workshopmongo/service/UserService.java
+++ src/main/java/com/felipesjc/workshopmongo/service/UserService.java
@@ -1,21 +1,27 @@
 package com.felipesjc.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipesjc.workshopmongo.domain.User;
 import com.felipesjc.workshopmongo.repository.UserRepository;
+import com.felipesjc.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
-		
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
