4836373c2fdaca8352a9a5a07a47cc93ba2e10bf
Felipesjc17 <felipe_sjc17@hotmail.com>
2021-12-29 19:00:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/felipesjc/workshopmongo/domain/Post.java
+++ src/main/java/com/felipesjc/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.felipesjc.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.felipesjc.workshopmongo.dto.AuthorDTO;
+import com.felipesjc.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments =new ArrayList<>();
 	
 	public Post() {
 	}
@@ -72,6 +77,16 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
+	
 	
 	@Override
 	public int hashCode() {
@@ -96,10 +111,6 @@ public class Post implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	
-	
-	
+	}	
 	
 }
